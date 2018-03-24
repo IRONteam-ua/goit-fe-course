86bc7835499afe99a7717048190f2b0440de4caf
Ihorbevz <web.ironteam@gmail.com>
2018-03-24 13:01:30 +0200

    add module_11

---
+++ html-css/Module-11/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp'), // Подключаем Gulp
+    sass = require('gulp-sass'); // Подключаем Sass пакет
+
+gulp.task('sass', function() { // Создаем таск "sass"
+  return gulp.src(['crs/sass/**/*.sass', 'crs/sass/**/*.scss']) // Берем источник
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
+    .pipe(gulp.dest('app/css')) // Выгружаем результата в папку css
+  });
+
+gulp.task('watch', function() {
+  gulp.watch(['crs/sass/**/*.sass', 'crs/sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
+});
+
+gulp.task('default', ['watch']);
